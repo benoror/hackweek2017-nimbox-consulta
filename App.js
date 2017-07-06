936ff96700ba33a9191a03cf1a776a5af870acf2
Ben Orozco <benoror@gmail.com>
2017-07-06 12:13:28 -0500

    Feat[swipe]: Add react-native-swiper
    https://medium.com/the-react-native-log/implement-snapchat-like-swipe-navigation-declaratively-in-react-native-309e71229c89

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
 import React from 'react';
+import Swiper from 'react-native-swiper';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import randomcolor from 'randomcolor'
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +10,65 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
+
+class TitleText extends React.Component {
+  render() {
+    return (
+      <Text style={{ fontSize: 48, color: 'white' }}>
+      {this.props.label}
+      </Text>
+    )
+  }
+}
+
+class Home extends React.Component {
+
+  viewStyle() {
+    return {
+      flex: 1,
+      backgroundColor: randomcolor(),
+      justifyContent: 'center',
+      alignItems: 'center',
+    }
+  }
+
+  render() {
+    return (
+      <Swiper
+      loop={false}
+      showsPagination={false}
+      index={1}>
+      <View style={this.viewStyle()}>
+      <TitleText label="Left" />
+      </View>
+      <Swiper
+      horizontal={false}
+      loop={false}
+      showsPagination={false}
+      index={1}>
+      <View style={this.viewStyle()}>
+      <TitleText label="Top" />
+      </View>
+      <View style={this.viewStyle()}>
+      <TitleText label="Home" />
+      </View>
+      <View style={this.viewStyle()}>
+      <TitleText label="Bottom" />
+      </View>
+      </Swiper>        
+      <View style={this.viewStyle()}>
+      <TitleText label="Right" />
+      </View>
+      </Swiper>
+
+    )
+  }
+}
+
+export default Home
