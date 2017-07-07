186da23bde918b80b99fcc00c064471128b259b3
Ben Orozco <benoror@gmail.com>
2017-07-06 20:39:07 -0500

    Feat: Bottom Sheet for IOS & Android
    https://github.com/cesardeazevedo/react-native-bottom-sheet-behavior
    https://github.com/eyaleizenberg/react-native-custom-action-sheet

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Swiper from 'react-native-swiper';
 import { StackNavigator } from 'react-navigation';
 import {
+  Platform,
   View,
   StyleSheet,
   Text,
   Button,
 } from 'react-native';
+
+import { CoordinatorLayout, BottomSheetBehavior} from 'react-native-bottom-sheet-behavior';
+import CustomActionSheet from 'react-native-custom-action-sheet';
 
 const styles = StyleSheet.create({
   container: {
@@ -57,12 +61,88 @@ const LeftScreen = StackNavigator({
   Nested: { screen: Nested },
 });
 
+const BottomSheetAndroid = () => (
+  <CoordinatorLayout style={{flex: 1}}>
+    <View style={{ flex: 1, backgroundColor: 'transparent' }}></View>
+    <BottomSheetBehavior
+      ref='bottomSheet'
+      peekHeight={70}
+      hideable={false}
+      state={BottomSheetBehavior.STATE_COLLAPSED}>
+      <View style={{backgroundColor: '#4389f2'}}>
+        <View style={{padding: 26}}>
+          <Text>Opcion A</Text>
+          <Text>Opcion B</Text>
+          <Text>Opcion C</Text>
+        </View>
+        <View style={{height: 200, backgroundColor: '#fff'}} />
+      </View>
+    </BottomSheetBehavior>
+  </CoordinatorLayout>
+);
+
+class BottomSheetIOS extends React.Component {
+  render() {
+    return (
+      <View>
+        <CustomActionSheet modalVisible={this.props.modalVisible} onCancel={this.props.closeModal}>
+          <View>
+            <Text>Opcion A</Text>
+            <Text>Opcion B</Text>
+            <Text>Opcion C</Text>
+          </View>
+        </CustomActionSheet>
+      </View>
+    );
+  }
+}
+
+class BottomSheet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { modalVisible: false };
+  }
+
+  openModal() {
+    this.setState({ modalVisible: true });
+  }
+
+  closeModal() {
+    this.setState({ modalVisible: false });
+  }
+
+  render() {
+    if(Platform.OS === 'android')
+      return (
+        <View>
+        <Button
+          onPress={() => this.openModal()}
+          title="Abrir BottomSheet"
+        />
+        <BottomSheetAndroid modalVisible={this.state.modalVisible} closeModal={this.closeModal.bind(this)} />
+        </View>
+      );
+    if(Platform.OS === 'ios')
+      return (
+        <View>
+        <Button
+          onPress={() => this.openModal()}
+          title="Abrir BottomSheet"
+        />
+        <BottomSheetIOS modalVisible={this.state.modalVisible} closeModal={this.closeModal.bind(this)} />
+        </View>
+      );
+  }
+};
+
 const Home = () => (
   <Swiper
   loop={false}
   showsPagination={false}
   index={1}>
+
     <LeftScreen />
+
     <Swiper
     horizontal={false}
     loop={false}
@@ -84,11 +164,14 @@ const Home = () => (
         </Text>
       </View>
     </Swiper>
+
     <View style={viewStyle()}>
       <Text>
         Right
       </Text>
+      <BottomSheet />
     </View>
+
   </Swiper>
 );
 
